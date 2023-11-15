89dc412f2b5af26fea1eed6eed470457be1f5d8f
Sawyer Cross <crosssawyer@gmail.com>
2023-11-15 14:12:48 -0600

    build: setup test dependencies
    added junit into library and junit test file

---
+++ src/project01/MainTest.java
@@ -0,0 +1,13 @@
+package project01;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class MainTest {
+    
+}
