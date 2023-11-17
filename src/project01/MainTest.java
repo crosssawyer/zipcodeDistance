5ab06d451e3c8e909a094de7adbcad6fec692ffc
Sawyer Cross <crosssawyer@gmail.com>
2023-11-17 09:44:06 -0600

    test: validated zip/miles functions
    wrote tests to check both passes and check fails (user types wrong input)

--- src/project01/MainTest.java
+++ src/project01/MainTest.java
@@ -8,6 +8,58 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
+
 public class MainTest {
+
+    public MainTest(){}
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testValidatezip(){
+        String zip = "64468";
+        boolean expected = true;
+        boolean result = Project01.validateZip(zip);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testValidatezipFail(){
+        String zip = "s64469";
+        boolean expected = false;
+        boolean result = Project01.validateZip(zip);
+        assertEquals(expected, result);
+    }
+
+        @Test
+    public void testValidateMiles(){
+        String miles = "60";
+        boolean expected = true;
+        boolean result = Project01.validateMiles(miles);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testValidateMilefail(){
+        String miles = "0";
+        boolean expected = false;
+        boolean result = Project01.validateMiles(miles);
+        assertEquals(expected, result);
+    }
+
     
 }
